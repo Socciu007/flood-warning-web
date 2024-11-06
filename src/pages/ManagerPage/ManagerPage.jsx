c209bcee07d5d1606957db77df8f9403211ec960
Money <manhtien310701@gmail.com>
2024-11-06 16:57:57 +0800

    add ui-manager

--- src/pages/ManagerPage/ManagerPage.jsx
+++ src/pages/ManagerPage/ManagerPage.jsx
@@ -1,90 +1,43 @@
 import React from "react";
-import { Card, Row, Col, Table, Progress } from "antd";
 import {
-  UserOutlined,
-  ShoppingCartOutlined,
-  DollarCircleOutlined,
+  DashboardFilled,
   BarChartOutlined,
+  UnorderedListOutlined,
 } from "@ant-design/icons";
-
+import NavbarComponent from "../../components/NavbarComponent/NavbarComponent";
+import { useTranslation } from "react-i18next";
+import "./style.scss";
+import locationIcon from "../../assets/icons/icon-location.svg";
+import userIcon from "../../assets/icons/icon-user.svg";
 const ManagerPage = () => {
-  // Data mẫu cho thống kê
-  const stats = [
-    {
-      title: "Tổng người dùng",
-      value: "350,897",
-      icon: <UserOutlined />,
-      color: "bg-danger",
-    },
-    {
-      title: "Doanh thu",
-      value: "924",
-      icon: <DollarCircleOutlined />,
-      color: "bg-warning",
-    },
-    {
-      title: "Đơn hàng",
-      value: "49.65%",
-      icon: <ShoppingCartOutlined />,
-      color: "bg-yellow",
-    },
-    {
-      title: "Hiệu suất",
-      value: "60%",
-      icon: <BarChartOutlined />,
-      color: "bg-info",
-    },
-  ];
+  const { t } = useTranslation();
 
   return (
     <div className="manager-page">
-      <div className="header bg-gradient-primary pb-8 pt-5 pt-md-8">
-        <div className="container-fluid">
-          <div className="header-body">
-            <Row gutter={[16, 16]}>
-              {stats.map((stat, index) => (
-                <Col span={6} key={index}>
-                  <Card className="card-stats mb-4 mb-xl-0">
-                    <div className="card-body">
-                      <Row>
-                        <div className="col">
-                          <h5 className="card-title text-uppercase text-muted mb-0">
-                            {stat.title}
-                          </h5>
-                          <span className="h2 font-weight-bold mb-0">
-                            {stat.value}
-                          </span>
-                        </div>
-                        <Col auto>
-                          <div
-                            className={`icon icon-shape ${stat.color} text-white rounded-circle shadow`}
-                          >
-                            {stat.icon}
-                          </div>
-                        </Col>
-                      </Row>
-                    </div>
-                  </Card>
-                </Col>
-              ))}
-            </Row>
-          </div>
-        </div>
+      <NavbarComponent />
+      <div className="navbar-content">
+        <ul className="navbar-list">
+          <li className="navbar-item">
+            <DashboardFilled />
+            <span>{t("Dashboard")}</span>
+          </li>
+          <li className="navbar-item">
+            <img src={locationIcon} alt="location" width={14} height={14} />
+            <span>{t("Areas List")}</span>
+          </li>
+          <li className="navbar-item">
+            <UnorderedListOutlined />
+            <span>{t("Examination List")}</span>
+          </li>
+          <li className="navbar-item">
+            <img src={userIcon} alt="user"/>
+            <span>{t("Users")}</span>
+          </li>
+        </ul>
       </div>
-
-      <div className="container-fluid mt--7">
-        <Row gutter={[16, 16]}>
-          <Col span={16}>
-            <Card title="Thống kê truy cập" className="shadow">
-              {/* Thêm biểu đồ ở đây */}
-            </Card>
-          </Col>
-          <Col span={8}>
-            <Card title="Thống kê đơn hàng" className="shadow">
-              {/* Thêm biểu đồ tròn ở đây */}
-            </Card>
-          </Col>
-        </Row>
+      <div className="main-content">
+        <div className="main-content-header"></div>
+        <div className="main-content-body"></div>
       </div>
     </div>
   );
